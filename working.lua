e307ffa0240a93ba7151fdd430f8f6b1dccfd77b
finalgirI <4everturtle@gmail.com>
2026-06-12 14:31:56 +0100

    Update working.lua

--- working.lua
+++ working.lua
@@ -34,7 +34,8 @@ local Data = {
 		["Bonnie Bennett"] = "114093297475680",
 		["Nora Hildegard"] = "131259403209726",
 		["Davina Claire"] = "109348032177998",
-		["Ashley"] = "99153543628823",
+		["Ashley"] = "99153543628823", 
+		["Qetsiyah"] = "105035246772721",
 	},
 
 	PhasmatosIncendia = {
