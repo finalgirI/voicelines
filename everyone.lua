aeefe2a20605f1b6c77dc0686b4900fbd3634b2c
finalgirI <4everturtle@gmail.com>
2026-06-28 21:18:52 +0100

    Update everyone.lua

--- everyone.lua
+++ everyone.lua
@@ -282,7 +282,9 @@ local SoundReplacements = {
 	["105998583954931"] = { Replacement = "13441892676", Volume = 3.5 }, -- Harae
 	["14043844852"] = { Replacement = "13904360117", Volume = 5 }, -- Heretic Joint Spell 
 	["120250468841070"] = { Replacement = "13904360117", Volume = 0 }, -- Expression Replacement
-	["132899449516141"] = { Replacement = "13904360117", Volume = 0 }, -- Brain Fry Qetsiyah Replacement
+	["132899449516141"] = {
+		["Qetsiyah"] = { Replacement = "13904360117", Volume = 0 }, -- Brain Fry Qetsiyah Replacement
+	},
 	["74468391415531"] = { Replacement = "16326825053", KeepPlayingSound = true, CasterSoundService = true }, -- Spiritual Cleanse
 	["116235007511881"] = "13203446447", -- Autem
 	["89008508391784"] = "17471844257", -- Hope's Repulse
