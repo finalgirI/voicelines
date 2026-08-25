1b5944885e750a04a2e929d506cecd2c53e7c992
finalgirI <4everturtle@gmail.com>
2026-08-25 11:11:59 +0100

    Update tvc.lua

--- tvc.lua
+++ tvc.lua
@@ -334,7 +334,7 @@ local SoundOverlays = {
 	["15254260885"] = {
 		["The Almighty"] = { Sound = "77919326748641", Volume = 2.5, DelayTime = 0.2 }, -- Lecutio
 	},
-	-- ["15237665151"] = { Sound = "87969470088924", Volume = 3.5, DelayTime = 0 }, -- Stopping Spell
+	["15237665151"] = { Sound = "87969470088924", Volume = 3.5, DelayTime = 0 }, -- Stopping Spell
 	-- Witch Abiltiies Voicelines:
 	["14518634071"] = {
 		["Psychic-Witch"] = { Sound = "118679634918055", Volume = 1.6, DelayTime = 0 }, -- Incendia'd
